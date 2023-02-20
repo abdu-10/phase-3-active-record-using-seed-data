a58179d5cf25b420a45823073f9205c5630de875
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-20 12:10:28 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding data..."
+50.times do 
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(10..50)
+    )
+end
+puts "Done!"
